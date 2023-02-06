76ac902a2fba47ec52b7168322f1981bed8bda1c
Jaret Kadlec <59940306+jaretkadlec@users.noreply.github.com>
2023-02-05 20:40:00 -0700

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,18 @@
+public class BinarySearch {
+    public boolean binarySearch(int[] arr, int searchPoint, int lastIdx, int firstIdx) {
+        int midIdx = (lastIdx-firstIdx)/2;
+        int comparePoint = arr[midIdx];
+        if (comparePoint < searchPoint) {
+            
+        } else if {
+
+        }
+        return arr;
+    }  
+    private static void main(args) {
+        int[] testArray = new int[10];
+        testArray = {0,1,2,3,4,5,6,7,8,9};
+        boolean searchExists = binarySearch(testArray,7,0,9)
+        StdOut.println("is number in array?")
+    }
+}
